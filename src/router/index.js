df8aa723925c7c9dd1a207029947c72b7c6c93b3
CXY0501 <chen.xy.2@foxmail.com>
2021-04-20 00:01:28 +0800

    20210419-01

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+// import style (<= Swiper 5.x)
+import 'swiper/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
 
 Vue.use(VueRouter)
 
