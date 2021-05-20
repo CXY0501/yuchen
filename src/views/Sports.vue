f861ad7b469e55abd382404e2ac19e29ca7e6d77
CXY0501 <chen.xy.2@foxmail.com>
2021-05-21 00:18:04 +0800

    20210521_01

--- src/views/Sports.vue
+++ src/views/Sports.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="sports">
     <div class="banner">
-      <embed
-        class="bannerVideo"
-        src="https://www.youtube.com/embed/mwqD4Cncdrc"
-        autostart="true"
-      />
+      <embed class="bannerVideo" :src="urlChosen" autostart="true" />
+    </div>
+    <div class="hockeyGames">
+      <h3>City League Season 2020/21</h3>
+      <ul>
+        <li
+          v-for="item in games"
+          :key="item.url"
+          @click="videoClick(item)"
+          :class="{ active: item.name === selected }"
+        >
+          {{ item.name }}
+        </li>
+      </ul>
     </div>
     <div></div>
   </div>
@@ -15,21 +24,75 @@
 export default {
   name: "Sports",
   data() {
-    return {};
+    return {
+      games: [
+        {
+          name: "City League SD8AA Lenovo vs Keewah 20210516",
+          url: "https://www.youtube.com/embed/5IY4sChY6bI",
+        },
+        {
+          name: "City League SD8AA Lenovo vs Keewah 20210509",
+          url: "https://www.youtube.com/embed/mwqD4Cncdrc",
+        },
+        {
+          name: "City League SD8AA Lenovo vs Keewah 20210502",
+          url: "https://www.youtube.com/embed/1w9H2ULgcnQ",
+        },
+        {
+          name: "City League SD8AA Lenovo vs Keewah 20210425",
+          url: "https://www.youtube.com/embed/tuRHB8uCW7s",
+        },
+        {
+          name: "City League SD8AA Lenovo vs Keewah 20210418",
+          url: "https://www.youtube.com/embed/WBzteFvQ-ac",
+        },
+        {
+          name: "City League SD8AA Lenovo vs Keewah 20210411",
+          url: "https://www.youtube.com/embed/-emP1ysgxB8",
+        },
+      ],
+      urlChosen: "https://www.youtube.com/embed/5IY4sChY6bI",
+      selected: "",
+    };
   },
-  methods: {},
+  methods: {
+    videoClick(item) {
+      this.urlChosen = item.url;
+      this.selected = item.name;
+      console.log(this.selected);
+      console.log(this.urlChosen);
+    },
+  },
 };
 </script>
 
 <style scoped>
 .banner {
-  width: 85%;
-  height: 500px;
-  margin: 62px auto;
+  width: 65%;
+  margin-left: 50px;
   background-color: lightblue;
 }
 .bannerVideo {
   width: 100%;
   height: 100%;
+}
+.banner,
+.hockeyGames {
+  float: left;
+  margin-top: 62px;
+  height: 500px;
+}
+.hockeyGames {
+  color: aliceblue;
+  width: 28%;
+  font-size: 15px;
+  background-color: rgba(0, 0, 0, 0.65);
+}
+.hockeyGames li {
+  list-style-type: none;
+  padding-top: 10px;
+}
+.hockeyGames ul {
+  padding-left: 0px;
 }
 </style>
