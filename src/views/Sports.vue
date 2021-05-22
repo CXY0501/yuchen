24f39ca6896e3bcee9611b242abf4b60aae995ab
CXY0501 <chen.xy.2@foxmail.com>
2021-05-22 22:51:48 +0800

    20210522_02

--- src/views/Sports.vue
+++ src/views/Sports.vue
@@ -172,4 +172,27 @@ export default {
 .score {
   margin-top: 20px;
 }
+/*chrome滚动条颜色设置*/
+*::-webkit-scrollbar {
+  width: 5px;
+  height: 10px;
+  background-color: transparent;
+}
+/*定义滚动条高宽及背景 高宽分别对应横竖滚动条的尺寸*/
+*::-webkit-scrollbar-track {
+  background-color: transparent;
+}
+/*定义滚动条轨道 内阴影+圆角*/
+*::-webkit-scrollbar-thumb {
+  background-color: skyblue;
+  border-radius: 5px;
+}
+
+/*定义滑块 内阴影+圆角*/
+.scrollbarHide::-webkit-scrollbar {
+  display: none;
+}
+.scrollbarShow::-webkit-scrollbar {
+  display: block;
+}
 </style>
